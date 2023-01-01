55c50848f160bdd48287b60ce5861eb3df45d19e
aemal55 <aemalahmad55@gmail.com>
2023-01-01 13:54:14 +0500

    Fonts and fonts color added
    The font for the splash screen translate has been added along with the color.

--- Russian To English Translator (React Native)/App.js
+++ Russian To English Translator (React Native)/App.js
@@ -1,84 +1,137 @@
-import 'react-native-gesture-handler'
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import SavedScreen from './screens/SavedScreen';
+import LanguageSelectScreen from './screens/LanguageSelectScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { AntDesign, Ionicons, MaterialIcons } from '@expo/vector-icons';
+import { Ionicons, AntDesign, MaterialIcons } from '@expo/vector-icons';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useEffect, useState } from 'react';
+import * as Font from 'expo-font';
+import colors from './utils/colors';
+
+
+SplashScreen.preventAutoHideAsync();
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
-        screenOptions={{ headerShown: false }}
-      <Tab.Screen 
+    <Tab.Navigator
+      screenOptions={{ headerShown: false }}>
+      <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          tabBarLabel: "Translate"
+          tabBarLabel: "Translate",
+          tabBarActiveTintColor: 'red',
+          tabBarInactiveTintColor: 'red',
+          tabBarIcon: (props) => <MaterialIcons name="g-translate" size={props.size} color={'black'} />
         }}
       />
 
-      <Tab.Screen 
+      <Tab.Screen
         name="Saved"
         component={SavedScreen}
         options={{
-          tabBarLabel: "Favourites"
+          tabBarLabel: "Favourites",
+          tabBarActiveTintColor: 'red',
+          tabBarInactiveTintColor: 'red',
+          tabBarIcon: (props) => <AntDesign name="heart" size={props.size} color={'black'} />
         }}
       />
 
-      <Tab.Screen 
+      <Tab.Screen
         name="Settings"
         component={SettingsScreen}
         options={{
-          tabBarLabel: "Settings"
+          tabBarLabel: "Settings",
+          tabBarActiveTintColor: 'red',
+          tabBarInactiveTintColor: 'red',
+          tabBarIcon: (props) => <Ionicons name="settings-sharp" size={props.size} color={'black'} />
         }}
       />
     </Tab.Navigator>
   )
 }
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
+
 export default function App() {
+
+  const [appIsLoaded, setAppIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await Font.loadAsync({
+          black: require("./assets/fonts//Roboto-Black.ttf"),
+          blackItalic: require("./assets/fonts/Roboto-BlackItalic.ttf"),
+          bold: require("./assets/fonts/Roboto-Bold.ttf"),
+          boldItalic: require("./assets/fonts/Roboto-BoldItalic.ttf"),
+          italic: require("./assets/fonts/Roboto-Italic.ttf"),
+          light: require("./assets/fonts/Roboto-Light.ttf"),
+          lightItalic: require("./assets/fonts/Roboto-LightItalic.ttf"),
+          medium: require("./assets/fonts/Roboto-Medium.ttf"),
+          mediumItalic: require("./assets/fonts/Roboto-MediumItalic.ttf"),
+          regular: require("./assets/fonts/Roboto-Regular.ttf"),
+          thin: require("./assets/fonts/Roboto-Thin.ttf"),
+          thinItalic: require("./assets/fonts/Roboto-ThinItalic.ttf"),
+        });
+      }
+      catch (e) {
+        console.log(e);
+      }
+      finally {
+        setAppIsLoaded(true);
+      }
+    };
+
+    prepare();
+
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (appIsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsLoaded]);
+  
+  if (!appIsLoaded) {
+    return null;
+  }
+
   return (
-  <NavigationContainer>
-    <View> style= {{ flex: 1 }}
-      <Stack.Navigator>
-        <Stack.Group>
-          <Stack.Screen 
-          name="Home"
-          component={TabNavigator}
-          options={{
-             headerTitle: "Translate",
-             tabBarIcon: (props) => <MaterialIcons name="g-translate" size={24} color={props.color} />
-          }}
-          />
-
-           <Stack.Screen 
-          name="Saved"
-          component={SavedScreen}
-          options={{
-             headerTitle: "Favourites",
-             tabBarIcon: (props) => <AntDesign name="heart" size={24} color={props.color} />
-            }}
-            />
-
-         <Stack.Screen 
-          name="Settings screen"
-          component={SettingsScreen}
-          options={{
-             headerTitle: "Settings",
-             tabBarIcon: (props) => <Ionicons name="settings-sharp" size={24} color={props.color} />
-            }}
-          />
-        </Stack.Group>
-      </Stack.Navigator>
-    </View>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <View onLayout={onLayout} style={{ flex: 1 }}>
+          <Stack.Navigator
+          screenOptions={{
+            headerTitleStyle: {
+              fontFamily: 'medium',
+              color: 'black'
+            },
+            headerStyle: {
+              backgroundColor: colors.primary
+            }
+          }}>
+            <Stack.Group>
+              <Stack.Screen
+                name="main"
+                component={TabNavigator}
+                options={{
+                  headerTitle: "Translate"
+                }}
+              />
+            </Stack.Group>
+            </Stack.Navigator>
+        </View>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
---
+++ Russian To English Translator (React Native)/utils/colors.js
@@ -0,0 +1,10 @@
+export default {
+    primary: '#E41B17',
+    primaryDisabled: 'red',
+    lightGrey: '#3B3B3B',
+    textColor: '#E41B17',
+    textColorDisabled: 'red',
+    greyBackground: '#131313',
+    subTextColor: '#131313'
+
+}
