9006daec9f8a65463090d0db278ba731db858a99
aemal55 <aemalahmad55@gmail.com>
2023-01-01 12:57:19 +0500

    Icons added
    Icons for translate, favourites and settings screen are added in the app.

--- Russian To English Translator (React Native)/App.js
+++ Russian To English Translator (React Native)/App.js
@@ -7,6 +7,7 @@ import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import SavedScreen from './screens/SavedScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign, Ionicons, MaterialIcons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -52,16 +53,27 @@ export default function App() {
           name="Home"
           component={TabNavigator}
           options={{
-             headerTitle: "Translate"
+             headerTitle: "Translate",
+             tabBarIcon: (props) => <MaterialIcons name="g-translate" size={24} color={props.color} />
           }}
           />
+
+           <Stack.Screen 
+          name="Saved"
+          component={SavedScreen}
+          options={{
+             headerTitle: "Favourites",
+             tabBarIcon: (props) => <AntDesign name="heart" size={24} color={props.color} />
+            }}
+            />
 
          <Stack.Screen 
           name="Settings screen"
           component={SettingsScreen}
           options={{
-             headerTitle: "Settings"
-          }}
+             headerTitle: "Settings",
+             tabBarIcon: (props) => <Ionicons name="settings-sharp" size={24} color={props.color} />
+            }}
           />
         </Stack.Group>
       </Stack.Navigator>
