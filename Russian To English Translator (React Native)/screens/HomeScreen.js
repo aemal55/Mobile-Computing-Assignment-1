185f190ebbc31c79805be574ab4d33ec8072f8f3
aemal55 <aemalahmad55@gmail.com>
2023-01-03 20:30:48 +0500

    API connection successful
    The API is connected and working for all languages.

--- Russian To English Translator (React Native)/screens/HomeScreen.js
+++ Russian To English Translator (React Native)/screens/HomeScreen.js
@@ -1,94 +1,126 @@
-import { Button, ProgressViewIOSComponent, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, TouchableOpacityBase, View } from 'react-native';
-import { AntDesign,Ionicons } from '@expo/vector-icons';
+import { ActivityIndicator, Button, FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 import colors from '../utils/colors';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import supportedLanguages from '../utils/supportedLanguages';
 
-export default function HomeScreen() {
-  const [enteredText, setEnteredText] = useState("");
-  const [resultText, setResultText] = useState("");
+
+
+export default function HomeScreen(props) {
+    const params = props.route.params || {};
+
+    const [enteredText, setEnteredText] = useState("");
+    const [resultText, setResultText] = useState("");
+    const [languageTo, setLanguageTo] = useState("en");
+    const [languageFrom, setLanguageFrom] = useState("ru");
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        if (params.languageTo) {
+            setLanguageTo(params.languageTo);
+        }
+
+        if (params.languageFrom) {
+            setLanguageFrom(params.languageFrom);
+        }
+    }, [params.languageTo, params.languageFrom]);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.LanguageContainer}>
-          <TouchableOpacity>
-              styles={styles.LanguageOption}
-              onPress={()  => props.navigation.navigate("LanguageSelect", {title: "Translate from"} )}
-              <Text style={styles.LanguageOptionText}>English</Text>
-          </TouchableOpacity>
+      <View style={styles.container}>
+        <View style={styles.languageContainer}>
+            <TouchableOpacity
+                style={styles.languageOption}
+                onPress={() => props.navigation.navigate("languageSelect", { title: "Translate from", selected: languageFrom, mode: 'from' })}>
+                <Text style={styles.languageOptionText}>{supportedLanguages[languageFrom]}</Text>
+            </TouchableOpacity>
 
-          <View style={styles.arrowContainer}>
-              <AntDesign name="arrowright" size={24} color={colors.lightGrey} />
-          </View>
+            <View style={styles.arrowContainer}>
+                <AntDesign name="arrowright" size={24} color={colors.lightGrey} />
+            </View>
 
-          <TouchableOpacity>
-              styles={styles.LanguageOption}
-              onPress={()  => props.navigation.navigate("LanguageSelect", {title: "Translate to"} )}
-              <Text style={styles.LanguageOptionText}>Russian</Text>
-          </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.languageOption}
+                onPress={() => props.navigation.navigate("languageSelect", { title: "Translate to", selected: languageTo, mode: 'to' })}>
+                <Text style={styles.languageOptionText}>{supportedLanguages[languageTo]}</Text>
+            </TouchableOpacity>
+        </View>
+
+        <View style={styles.inputContainer}>
+            <TextInput
+                multiline
+                placeholder='Enter your text here'
+                placeholderTextColor={'white'}
+                style={styles.textInput}
+                onChangeText={(text) => setEnteredText(text)}
+            />
+
+            <TouchableOpacity
+                onPress={isLoading ? undefined : onSubmit}
+                disabled={enteredText === ""}
+                style={styles.iconContainer}>
+
+                {
+                    isLoading ?
+                    <ActivityIndicator size={'small'} color={colors.primary} /> :
+                    <Ionicons 
+                        name="arrow-redo-circle-sharp"
+                        size={24} 
+                        color={enteredText !== "" ? colors.primary : colors.primaryDisabled} />
+                }
+                
+            </TouchableOpacity>
+        </View>
+
+        <View style={styles.resultContainer}>
+            <Text style={styles.resultText}>{resultText}</Text>
+
+            <TouchableOpacity
+                onPress={copyToClipboard}
+                disabled={resultText === ""}
+                style={styles.iconContainer}>
+                <Ionicons 
+                    name="ios-copy"
+                    size={24} 
+                    color={resultText !== "" ? colors.textColor : colors.textColorDisabled} />
+            </TouchableOpacity>
+        </View>
+
+        <View style={styles.historyContainer}>
+            <FlatList
+                data={history.slice().reverse()}
+                renderItem={itemData => {
+                    return <TranslationResult itemId={itemData.item.id} />
+                }}
+            />
+        </View>
       </View>
-
-      <View style={styles.inputContainer}>
-          <TextInput 
-              multiline
-              placeholder='Enter your text here'
-              style={styles.TextInput}
-              onChangeText={(text) => setEnteredText(text)}
-       /> 
-       
-       <TouchableOpacity>
-           disabled={enteredText === ""}
-           style={styles.iconContainer}
-           <Ionicons 
-           name="arrow-redo-circle-sharp" 
-           size={24} 
-           color={enteredText !=="" ? colors.primary : colors.primaryDisabled} />
-       </TouchableOpacity>     
-      </View>
-
-      <View style={styles.resultContainer}>
-          <Text style={styles.resultText}>{resultText}</Text>
-
-          <TouchableOpacity>
-           disabled={resultText === ""}
-           style={styles.iconContainer}
-           <Ionicons 
-           name="ios-copy" 
-           size={24} 
-           color={resultText !=="" ? colors.textColor : colors.textColorDisabled} />
-       </TouchableOpacity>  
-      </View>
-
-      <View style={styles.historyContainer}>
-
-      </View>
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    },
-    LanguageContainer: {
-      flexDirection: 'row',
-      borderBottomColor: colors.lightGrey,
-      borderBottomWidth: 1
-    },
-    LanguageOption: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 15
-    },
-    arrowContainer: {
-      width: 50,
-      alignItems: 'center',
-      justifyContent: 'center',
+    backgroundColor: '#131313',
   },
-  LanguageOptionText: {
+  languageContainer: {
+    flexDirection: 'row',
+    borderBottomColor: colors.lightGrey,
+    borderBottomWidth: 1
+  },
+  languageOption: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 15
+  },
+  arrowContainer: {
+    width: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  languageOptionText: {
     color: colors.primary,
-    fontfamily: 'regular',
+    fontFamily: 'regular',
     letterSpacing: 0.3
   },
   inputContainer: {
@@ -96,15 +128,15 @@ const styles = StyleSheet.create({
     borderBottomColor: colors.lightGrey,
     borderBottomWidth: 1
   },
-  TextInput: {
-   flex: 1,
-   marginTop: 10,
-   paddingHorizontal: 20,
-   paddingVertical: 15,
-   fontFamily: 'regular',
-   letterSpacing: 0.3,
-   height: 90,
-   color: colors.textColor
+  textInput: {
+    flex: 1,
+    marginTop: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    fontFamily: 'regular',
+    letterSpacing: 0.3,
+    height: 90,
+    color: colors.textColor
   },
   iconContainer: {
     paddingHorizontal: 10,
@@ -112,11 +144,11 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   resultContainer: {
-   borderBottomColor: colors.lightGrey,
-   borderBottomWidth: 1,
-   flexDirection: 'row',
-   height: 90,
-   paddingVertical: 15
+    borderBottomColor: colors.lightGrey,
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    height: 90,
+    paddingVertical: 15
   },
   resultText: {
     fontFamily: 'regular',
@@ -128,8 +160,6 @@ const styles = StyleSheet.create({
   historyContainer: {
     backgroundColor: colors.greyBackground,
     flex: 1,
-    padding: 10,
-
+    padding: 10
   }
-  
 });
